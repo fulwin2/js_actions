58836c660a92ddba82c3c46ebb3305b7bc674966
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2023-12-26 13:01:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  //const nameToGreet = core.getInput('who-to-greet');
+  //console.log(`Hello ${nameToGreet}!`);
 
-const args = process.argv.slice(2); // The first two elements are 'node' and the script name
+  const args = process.argv.slice(2); // The first two elements are 'node' and the script name
 
 
-if (args.length === 0) {
-  console.error('Error: Please provide a value as a command line argument.');
-  process.exit(1); // Exit with an error code
-}
+  if (args.length === 0) {
+    console.error('Error: Please provide a value as a command line argument.');
+    process.exit(1); // Exit with an error code
+  }
 
-const inputValue = args[0]; // Assuming you want the first argument
+  const inputValue = args[0]; // Assuming you want the first argument
 
-console.log(`Input Value from Command Line: ${inputValue}`);
+  console.log(`Input Value from Command Line: ${inputValue}`);
   
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
